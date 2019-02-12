11aa574d4560fd0c53b198d458fc732f1d7e7447
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2019-02-11 22:04:50 -0600

    Updates

---
+++ precache-manifest.c8d93c2615604df9affafa91cee31c08.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0214d2bc85162e73ca439bb65e3f6e8",
+    "url": "/ClickyGame/static/media/Gene.f0214d2b.jpg"
+  },
+  {
+    "revision": "da6fca84e08069eb56cb",
+    "url": "/ClickyGame/static/css/main.b36b4814.chunk.css"
+  },
+  {
+    "revision": "fa842ade0c2b87568f9a",
+    "url": "/ClickyGame/static/js/1.fa842ade.chunk.js"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "ba946fb209edc78db62fa60364d63147",
+    "url": "/ClickyGame/static/media/Bob.ba946fb2.jpg"
+  },
+  {
+    "revision": "ee0e863468c441bb5f8392d38ca89696",
+    "url": "/ClickyGame/static/media/CalvinFishodor.ee0e8634.jpg"
+  },
+  {
+    "revision": "7777cb8ea7a94bb4122de8431a9b7099",
+    "url": "/ClickyGame/static/media/Darryl.7777cb8e.jpg"
+  },
+  {
+    "revision": "da6fca84e08069eb56cb",
+    "url": "/ClickyGame/static/js/main.da6fca84.chunk.js"
+  },
+  {
+    "revision": "188b2e1c6160d5fea9bd5f75bf1e15da",
+    "url": "/ClickyGame/static/media/JimmyJr.188b2e1c.jpg"
+  },
+  {
+    "revision": "984cf73e29062f2e5fa0fd8443610812",
+    "url": "/ClickyGame/static/media/Linda.984cf73e.jpg"
+  },
+  {
+    "revision": "69c29f678980937b9da13912177d442f",
+    "url": "/ClickyGame/static/media/Louise.69c29f67.jpeg"
+  },
+  {
+    "revision": "fe20e7645e80547c0b8028d72ad9bfa3",
+    "url": "/ClickyGame/static/media/Zeke.fe20e764.jpg"
+  },
+  {
+    "revision": "f82959f9d7eeba6a73c855407b2ba517",
+    "url": "/ClickyGame/static/media/gayle.f82959f9.jpg"
+  },
+  {
+    "revision": "cd50be7e9b9e4bee5e6b3211fc510bcf",
+    "url": "/ClickyGame/index.html"
+  }
+];
